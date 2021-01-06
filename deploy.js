fc04117954373cd4420c5d4dc3041ddac9e8f307
Wolfer336 <luis97v@hotmail.com>
2021-01-05 17:24:39 -0800

    new updated

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'young effort already frozen earn lunch develop purchase salute stable picture long',
+  'https://rinkeby.infura.io/v3/66c4a104b5ed4c29bc83384e9313b253'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAcoounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] })
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
